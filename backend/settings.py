b9a54d84540a50247ac7e45e0f50f101fe288aee
Dhanush V <dhanush.v0724@gmail.com>
2025-07-07 12:13:16 +0530

    feat: add static home.html landing page with Postman usage instructions

--- backend/settings.py
+++ backend/settings.py
@@ -74,6 +74,22 @@ TEMPLATES = [
 WSGI_APPLICATION = "backend.wsgi.application"
 
 
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"],  # ✅ this line
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                 'django.template.context_processors.debug',
+                'django.template.context_processors.request',  # ✅ required for admin sidebar
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
